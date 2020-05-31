38820cee43744b635181b6969df57477fa7cd76a
c1b <c1b@users.noreply.github.com>
2020-05-31 01:07:05 -0700

    Updates

---
+++ precache-manifest.8ce72ca0d4311a41e4f58b6b4c5a1aa4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "076155708840ecbdf09ced020dcb94b5",
+    "url": "/movieapp/index.html"
+  },
+  {
+    "revision": "f889e2673791b04b857c",
+    "url": "/movieapp/static/css/2.3aa4258e.chunk.css"
+  },
+  {
+    "revision": "419e2a20bf2fec3a6dd5",
+    "url": "/movieapp/static/css/main.91d801dc.chunk.css"
+  },
+  {
+    "revision": "f889e2673791b04b857c",
+    "url": "/movieapp/static/js/2.efca4033.chunk.js"
+  },
+  {
+    "revision": "a0bfa271ba0a4ae866e1af596107ab2f",
+    "url": "/movieapp/static/js/2.efca4033.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "419e2a20bf2fec3a6dd5",
+    "url": "/movieapp/static/js/main.02b50dfd.chunk.js"
+  },
+  {
+    "revision": "78941574863ad5004594",
+    "url": "/movieapp/static/js/runtime-main.6cac2083.js"
+  }
+]);
